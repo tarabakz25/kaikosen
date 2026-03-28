a9e680f2a8f7176f33830b8f33893fbff337c56e
Kizuki Aiki <aikikizuki@gmail.com>
2026-03-28 12:39:16 +0900

    fix: use process.env for DATABASE_URL in configuration

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,8 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = postgres(env.DATABASE_URL, { prepare: false });
+const client = postgres(process.env.DATABASE_URL!, { prepare: false });
 
 export const db = drizzle(client, { schema });
